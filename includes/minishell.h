5a823410f0f2702f07579ebdc004c82596626a6a
meunostu <mihdevel@gmail.com>
2021-05-02 07:13:22 +0300

    Created (main.c parser.c utils.c minishell.h, Makefile .gitignore) 02052021_0710

---
+++ includes/minishell.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meunostu <meunostu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/02 05:36:17 by meunostu          #+#    #+#             */
+/*   Updated: 2021/05/02 06:43:14 by meunostu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h> // Delete
+
+typedef struct	s_mini
+{
+	int			i;
+}				t_mini;
+/*
+** MINISHELL
+*/
+
+/*
+** PARSING
+*/
+
+/*
+** UTILS
+*/
+
+#endif //MINISHELL_H
