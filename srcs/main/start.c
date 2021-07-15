7162922d057f320d16ca0d20f6710a6094de6654
meunostu <mihdevel@gmail.com>
2021-07-15 20:28:24 +0300

    Merge from areggie 210715_2030

--- srcs/main/start.c
+++ srcs/main/start.c
@@ -6,7 +6,7 @@
 /*   By: meunostu <meunostu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/02 05:42:41 by meunostu          #+#    #+#             */
-/*   Updated: 2021/07/15 20:20:04 by meunostu         ###   ########.fr       */
+/*   Updated: 2021/07/15 20:27:36 by meunostu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,29 +103,18 @@ int	main(int ac, char **av, char **env)
 	signal(SIGINT, &ctrl_c);
 	while (1)
 	{
-		string = readline("minishell: ");
+		string = readline("minishell> ");
 		if (!string)
 			ctrl_d(131);
 		else if (*string)
 			add_history(string);
 		parser(&main, string);
 		mem_free(&string);
-        if (main.job->pipe->redir->command && !main.job->pipe->redir->error && main.job->num_pipes == 0 &&
-        main.job->pipe->redir->redir_type == ERROR)
-            process_builtins_and_divide_externals(&main);
-        if (main.job->num_pipes != 0 && main.job->pipe->redir->redir_type == ERROR)
-            execute_pipes(&main);
-        if (main.job->pipe->redir->redir_type != ERROR)
-        {
-            if (main.job->pipe->redir->redir_type == OUTPUT)
-                redir_one_right(&main);
-            if (main.job->pipe->redir->redir_type == APPEND_OUTPUT)
-                redir_two_right(&main);
-            if (main.job->pipe->redir->redir_type == INPUT)
-                redir_one_left(&main);
-            if (main.job->pipe->redir->redir_type == INPUT_MULTILINE)
-                redir_two_left(&main);
-        }
+		if (main.job->pipe->redir->command && !main.job->pipe->redir->error
+			&& main.job->num_pipes == 0)
+			process_builtins_and_divide_externals(&main);
+		if (main.job->num_pipes != 0)
+			execute_pipes(&main);
 		end_session(&main);
 	}
 	av[ac] = env[ac];
